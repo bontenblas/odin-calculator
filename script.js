4180888257890fcfc853334ae0d8875fff3ca6dc
bontenblas <kengrlao@gmail.com>
2026-06-26 20:16:52 +0800

    properly working fundamental functions

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ function divide(a, b) {
 }
 
 function operate(num1, ope, num2) {
+    num1 = parseInt(num1)
+    num2 = parseInt(num2)
+
     if (ope === '+') {
         return add(num1, num2)
     }
@@ -32,61 +35,83 @@ function operate(num1, ope, num2) {
     }
 }
 
-// TODO:
-//   - fix refresh when user presses C button
-//   - fix equals not working
-//   - figure out the secondNum problem
-
-
-let currentNum = ''
-let initOperand = ''
+let currNum = ''
+let currOperand = ''
 let firstNum = ''
 let operator = ''
 let secondNum = ''
+let result = ''
 const display = document.querySelector('.display p')
 const buttons = document.querySelectorAll('.button')
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         if (button.classList.contains('clear')) {
-            currentNum = ''
+            currNum = ''
+            currOperand = ''
+            firstNum = ''
+            operator = ''
+            secondNum = ''
             display.textContent = 0
+            console.log(firstNum)
         } else if (button.classList.contains('operator')) {
-            if (currentNum === '') {
+            if (currOperand === '' && currNum === '') {
+                console.log('no currNum so nothing happened')
                 return
             } else {
                 const getOpe = button.textContent
-                initOperand = currentNum + ' ' + getOpe + ' '
-                operator = getOpe
-                display.textContent = initOperand
+                currOperand = currNum + ' ' + getOpe + ' '
+                display.textContent = currOperand
+                console.log('operator added')
                 
                 if (firstNum === '') {
-                    firstNum = currentNum
+                    firstNum = currNum
+                    console.log('saving firstNum')
                     console.log(firstNum)
-                    console.log(initOperand)
-                    currentNum = ''
-                } 
-                // else {
-                //     secondNum = currentNum
-                //     console.log(secondNum)
-                // }
+                    console.log(currOperand)
+                    operator = getOpe
+                    currNum = ''
+                } else if (secondNum === '') {
+                    currOperand = firstNum + ' ' + getOpe + ' '
+                    operator = getOpe
+                    display.textContent = currOperand
+                    console.log('changed/kept same operator. nothing happened')
+                    console.log(currOperand)
+                } else {
+                    result = operate(firstNum, operator, secondNum)
+                    firstNum = result
+                    currOperand = firstNum + ' ' + getOpe + ' '
+                    display.textContent = currOperand
+                    operator = getOpe
+                    secondNum = ''
+                    currNum = ''
+                }
             }
-        }  else if (button.classList.contains('equals')) {
-            if (currentNum === '' || firstNum === '' || secondNum === '' || operator === '') {
+        }  else if (button.classList.contains('equal')) {
+            console.log('pressed equals')
+            if (currNum === '' || firstNum === '' || secondNum === '' || operator === '') {
+                console.log('nothing happened')
                 return
             } else {
-                secondNum = currentNum
+                console.log('something happened')
                 console.log(secondNum)
-                display.textContent = operate(firstNum, operator, secondNum)
+                result = operate(firstNum, operator, secondNum)
+                display.textContent = result
+                currNum = ''
+                firstNum = result
+                secondNum = ''
             }
         } else {
             const getValue = button.textContent
             if (firstNum === '') {
-                currentNum = currentNum + getValue;
-                display.textContent = currentNum;
+                currNum = currNum + getValue;
+                display.textContent = currNum;
+                console.log('adding to firstNum set')
             } else {
-                currentNum = currentNum + getValue
-                display.textContent = initOperand + currentNum
+                currNum = currNum + getValue
+                secondNum = currNum
+                display.textContent = currOperand + currNum
+                console.log('adding to secondNum set')
             }
         }
     })
